578c41c7bfa9033fbd65f3a52fcbbcbc48cb1374
Branko Collin <collin@xs4all.nl>
2023-07-31 19:20:18 +0200

    Added in the games log export function, mostly a copy from the MVP, but I removed some extra wordy code.

--- src/components/games-log.vue
+++ src/components/games-log.vue
@@ -29,6 +29,38 @@
 			maxItems.value = props.gamesLogItems.length;
 		}
 	}
+	
+	function exportGamesLog(format) {
+		console.log('exporting ...');
+		if (format === undefined) {
+			format = 'txt'; // For now the only format, future JSON?
+		}
+
+		var logString = '';
+
+		if (format === 'txt') {
+			var max = props.gamesLogItems.length;
+			for (var i = 0; i < max; i++) {
+				logString += parseDisplayLogItem(props.gamesLogItems[i]) + String.fromCharCode(10);
+			}
+		}
+
+		var now = new Date();
+		var dateString = now.getFullYear() + '-' + String(1+now.getMonth()).padStart(2, '0') + '-' + String(now.getDate()).padStart(2, '0');
+
+		saveData(logString, '7wonder-duel-results-' + dateString + '.txt');
+	}
+	
+	function saveData(data, fileName) {
+		var a = document.createElement('a');
+		var blob = new Blob([data], {type: "octet/stream"});
+		var url = window.URL.createObjectURL(blob);
+		a.href = url;
+		a.download = fileName;
+		a.click();
+		window.URL.revokeObjectURL(url);
+		a.remove();
+	}
 
 	function parseDisplayLogItem(item) {
 		if (typeof item !== 'object') { return ''; }
@@ -51,7 +83,9 @@
 			<li>{{parseDisplayLogItem(item)}}</li>
 		</ul>
 		
-		<p v-if="maxItems < gamesLogItems.length" @click="upMaxItems"><button>Show more</button></p>
+		<p v-if="maxItems < gamesLogItems.length"><button @click="upMaxItems">Show more</button></p>
+		
+		<p v-if="gamesLogItems.length > 1"><button @click="exportGamesLog()">Export games log</button></p>
 	</div>
 </template>
 
