31271f3e6f83257ab1e380f341167495bdc39130
Branko Collin <collin@xs4all.nl>
2023-08-02 14:32:00 +0200

    Added PWA assets. There is a small problem here, 'npm run build' (using Vite-PWA) appears to fail to copy all but one of these assets to the 'dist' folder. I am leaving this as is for now, as the goal of making this app was to teach myself a bit of Vue, not wrangle its many plug-ins.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestOptions = {
+	name: '7 Wonders Duel Scoring',
+	short_name: '7WD Scoring',
+	description: 'Calculate your 7 Wonders Duel scores.',
+	theme_color: '#ffffff',
+	icons: [
+		{
+			src: 'pwa-192x192.png',
+			sizes: '192x192',
+			type: 'image/png'
+		},
+		{
+			src: 'pwa-512x512.png',
+			sizes: '512x512',
+			type: 'image/png'
+		}
+	]
+};
+
+const pWAConfig = {
+	manifest: manifestOptions,
+	minify: false,
+	registerType: 'autoUpdate'
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [
+		vue(),
+		VitePWA(pWAConfig)
+	],
+	base: "",
 })
+
